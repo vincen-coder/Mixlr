fdcc75b08bd0bc618256db5c82707dec8a111768
vincen-coder <omoseebivincent185@gmail.com>
2026-03-10 16:32:49 +0100

    Renamed the hooks

--- src/components/Recorder.jsx
+++ src/components/Recorder.jsx
@@ -2,8 +2,8 @@ function Recorder({ isRecording, recordedAudio, startRecording, stopRecording })
   return (
     <div>
       {isRecording
-        ? <button onClick={stopRecording}>Stop Recording</button>
-        : <button onClick={startRecording}>Start Recording</button>
+        ? <button className="text-blue-500" onClick={stopRecording}>Stop Recording</button>
+        : <button className="text-blue-500" onClick={startRecording}>Start Recording</button>
       }
 
       {recordedAudio && (
@@ -11,7 +11,7 @@ function Recorder({ isRecording, recordedAudio, startRecording, stopRecording })
           <p>Recording done!</p>
           <audio controls src={recordedAudio} />
           <a href={recordedAudio} download="mixlr-recording.wav">
-            <button>Download Recording</button>
+            <button className="text-blue-500">Download Recording</button>
           </a>
         </div>
       )}
